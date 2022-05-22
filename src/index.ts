c5b485723b1210a7840d172501e66774e8f5e4b3
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-21 21:05:44 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+/*Ejercicio – Aumento de Sueldo
+• Una empresa desea premiar a sus empleados con un
+aumento de sueldo. Este aumento se ajusta a la siguiente
+tabla:
+58
+Sueldo Actual Sueldo con Aumento
+0 - 15.000 $ 20%
+15.001 - 20.000 $ 10%
+20.001 - 25.000 $ 5%
+Más de 25.000 $ No hay aumento
+• Desarrolle un algoritmo dado el salario actual de un
+empleado determine el aumento de sueldo a aplicar y
+se lo muestre*/
+
+
+
 let sueldo = document.getElementById("dato1");
 let btnEnviar = document.getElementById("enviar");
 let rotulo1 = document.getElementById("rotulo1");
